53e72e723a9418424b0b0c118f8071312bbb626e
Jose Browne <josebrowne@gmail.com>
2019-06-16 23:27:52 -0400

    Custom 404 Redirects

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,30 @@
-import React from "react";
-import { Link } from "gatsby";
+import React, { useEffect } from "react";
+import { Link, navigate } from "gatsby";
 import { Layout } from "../components/common";
 
-const NotFoundPage = () => (
-  <Layout>
-    <div className="container">
-      <article className="content" style={{ textAlign: `center` }}>
-        <h1 className="content-title">Error 404</h1>
-        <section className="content-body">
-          Page not found, <Link to="/">return home</Link> to start over
-        </section>
-      </article>
-    </div>
-  </Layout>
-);
+const NotFoundPage = () => {
+  const path = window.location.pathname;
+
+  // Redirect /open/* -> /*
+  if (path.startsWith(`/open`)) {
+    useEffect(() => {
+      const newPath = window.location.pathname.replace(`/open`, ``);
+      navigate(newPath);
+    }, []);
+  }
+
+  return (
+    <Layout>
+      <div className="container">
+        <article className="content" style={{ textAlign: `center` }}>
+          <h1 className="content-title">Error 404</h1>
+          <section className="content-body">
+            Page not found, <Link to="/">return home</Link> to start over
+          </section>
+        </article>
+      </div>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
