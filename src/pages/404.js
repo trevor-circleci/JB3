e44183ac543acacb4b68206be6efc27c1af2a522
Jose Browne <josebrowne@gmail.com>
2019-06-19 18:17:57 -0400

    Cleanup Redirects :)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,36 +1,18 @@
-import React, { useEffect } from "react";
-import { Link, navigate } from "gatsby";
+import React from "react";
+import { Link } from "gatsby";
 import { Layout } from "../components/common";
 
-const NotFoundPage = () => {
-  try {
-    if (window) {
-      const path = window.location.pathname;
-
-      // Redirect /open/* -> /*
-      if (path.startsWith(`/open`)) {
-        useEffect(() => {
-          const newPath = window.location.pathname.replace(`/open`, ``);
-          navigate(newPath);
-        }, []);
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
-  return (
-    <Layout>
-      <div className="container">
-        <article className="content" style={{ textAlign: `center` }}>
-          <h1 className="content-title">Error 404</h1>
-          <section className="content-body">
-            Page not found, <Link to="/">return home</Link> to start over
-          </section>
-        </article>
-      </div>
-    </Layout>
-  );
-};
+const NotFoundPage = () => (
+  <Layout>
+    <div className="container">
+      <article className="content" style={{ textAlign: `center` }}>
+        <h1 className="content-title">Error 404</h1>
+        <section className="content-body">
+          Page not found, <Link to="/">return home</Link> to start over
+        </section>
+      </article>
+    </div>
+  </Layout>
+);
 
 export default NotFoundPage;
